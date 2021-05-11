9b3ce0c4382aa73a7f2b0f01b2343be57c44d609
Murtaja <murtajaadnan7@gmail.com>
2021-05-12 01:02:42 +0300

    Refactor: refactor rating

--- src/components/Rating.vue
+++ src/components/Rating.vue
@@ -3,94 +3,78 @@
 		<div>
 			<strong>{{ ave_rating }}</strong>
 		</div>
-		<!-- maybe put this one below with it children -->
-		<b-form-rating
-			v-if="refresh !== false"
-			v-b-modal.modal-center
-			class="rating-posit rating-rotate col-12"
-			:value="ave_rating"
-			readonly
-			variant="warning"
-			inline
-			no-border
-		></b-form-rating>
 
-		<b-form-rating
-			v-else
-			id="popover"
-			locale="ar-EG"
-			class="rating-posit col-12"
-			:value="ave_rating"
-			readonly
-			variant="warning"
-			inline
-			no-border
-		></b-form-rating>
-
-		<b-popover
-			target="popover"
-			placement="topleft"
-			variant="info"
-			v-if="!refresh"
-		>
-			يرجىء
-			<b-button variant="outline-primary" size="sm" to="/login"
-				>تسجل الدخول</b-button
-			>
-			أولا
-		</b-popover>
-
-		<b-modal id="modal-center" centered title="التقييم" hide-header>
-			<p class="my-4 text-right">
-				لتقييم <strong>{{ this.arb_name }}</strong> أظغط على احد النجوم!
-			</p>
+		<div @click="handleShowModal">
 			<b-form-rating
-				class="col-4 give-rating-posit"
+				id="popover"
 				locale="ar-EG"
-				v-model="rate"
+				class="col-12"
+				:value="ave_rating"
+				readonly
 				variant="warning"
-				icon-clear="x-circle"
-				style="font-size: 2rem;"
-				show-clear
 				inline
 				no-border
 			></b-form-rating>
-			<template #modal-footer="{ ok, cancel }">
-				<b-button
-					size="sm"
-					:disabled="rate === null"
-					class="btn-posit ml-0"
-					variant="success"
-					@click="ok(), giveRating()"
-				>
-					ارسال
-				</b-button>
-				<b-button
-					size="sm"
-					class="btn-posit"
-					variant="danger"
-					@click="cancel()"
-				>
-					ألغاء
-				</b-button>
-			</template>
-		</b-modal>
+		</div>
 		<div>
 			<p class="rating-font">
 				للتقييم اضغط على النجوم!
 			</p>
 		</div>
+		<b-popover target="popover" placement="topleft" variant="info" v-if="!refresh">
+			يرجىء
+			<b-button variant="outline-primary" size="sm" to="/login">تسجل الدخول</b-button>
+			أولا
+		</b-popover>
+
+		<b-modal v-model="modalShow" centered title="التقييم" hide-header hide-footer>
+			<b-form-row>
+				<b-col cols="12">
+					<p class="my-4 text-right">
+						لتقييم <strong>{{ this.arb_name }}</strong> أظغط على احد النجوم!
+					</p></b-col
+				>
+				<b-col cols="12" align="center">
+					<b-form-rating
+						locale="ar-EG"
+						v-model="rate"
+						variant="warning"
+						icon-clear="x-circle"
+						class="stars"
+						show-clear
+						inline
+						no-border
+					></b-form-rating
+				></b-col>
+				<b-col cols="12">
+					<b-button
+						size="sm"
+						:disabled="rate === null"
+						class="ml-2"
+						variant="success"
+						@click="giveRating"
+					>
+						ارسال
+					</b-button>
+					<b-button size="sm" variant="danger" @click="modalShow = false">
+						ألغاء
+					</b-button>
+				</b-col></b-form-row
+			>
+		</b-modal>
 	</div>
 </template>
 
 <script>
 import { mapState } from "vuex"
 import shared from "../shared"
+
 export default {
 	data() {
 		return {
-			ave_rating: null,
-			rate: null
+			ave_rating: 0,
+			rate: null,
+			modalShow: false
 		}
 	},
 	props: {
@@ -99,15 +83,13 @@ export default {
 		arb_name: String
 	},
 	computed: mapState({
-		refresh: (state) => state.tokenModel.refresh,
-		username: (state) => state.tokenModel.username,
-		url: (state) => state.tokenModel.url
+		refresh: (state) => state.tokenModel.refresh
 	}),
 	methods: {
 		fetchAveRating() {
 			shared.fetchData(`${this.sub_url + this.id}&page_size=1`).then((res) => {
 				// in case no results comes back.
-				if (res.results.length) {
+				if (res.count) {
 					this.ave_rating = res.results[0].ave_rating
 				} else {
 					this.ave_rating = 0
@@ -129,11 +111,17 @@ export default {
 				.then((data) => {
 					this.ave_rating = data.ave_rating
 					this.rate = 0
+					this.fetchAveRating()
+					this.modalShow = false
 				})
 				.catch((err) => {
 					console.log(err)
 				})
-			this.fetchAveRating()
+		},
+		handleShowModal() {
+			if (this.refresh != false) {
+				this.modalShow = true
+			}
 		}
 	},
 	mounted() {
@@ -143,20 +131,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.give-rating-posit {
-	position: relative;
-	left: 220px;
-	// transform: rotateY(180deg);
+::v-deep .form-control:focus {
+	box-shadow: none;
 }
-.rating-rotate {
-	transform: rotateY(180deg);
+.stars {
+	font-size: 2rem;
 }
 .rating-font {
 	font-size: 10px;
-}
-
-.val-posit {
-	position: relative;
-	right: 125px;
 }
 </style>
