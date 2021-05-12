b03ec02afce3d66a527e7d52512d28170c99e4ec
Murtaja <murtajaadnan7@gmail.com>
2021-05-12 16:45:11 +0300

    Refactor: refactor navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,58 +1,47 @@
 <template>
-	<div dir="rtl">
-		<b-navbar toggleable="lg" type="dark" variant="info">
-			<!-- :icon="['fas', 'search']" -->
+	<b-navbar toggleable="lg" type="dark" variant="info">
+		<b-navbar-brand class="desk-search">
+			<search />
+		</b-navbar-brand>
+		<b-navbar-brand to="/">الرئيسية</b-navbar-brand>
 
-			<b-navbar-brand class="desk-search">
-				<!-- <font-awesome-icon v-b-modal.modal-1 icon="search" size="lg" />
+		<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-        <b-modal id="modal-1" centered hide-footer hide-header> -->
-				<search />
-				<!-- </b-modal> -->
-			</b-navbar-brand>
-			<b-navbar-brand to="/">الرئيسية</b-navbar-brand>
+		<!-- to be right aligned in mobile -->
+		<b-collapse id="nav-collapse" is-nav align="right">
+			<b-navbar-nav>
+				<b-nav-item to="/news">الأخبار</b-nav-item>
+				<b-nav-item to="/university">الجامعة</b-nav-item>
+			</b-navbar-nav>
 
-			<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+			<!-- Right aligned nav items -->
+			<b-navbar-nav class="mr-auto">
+				<b-navbar-brand class="pl-5 mob-search">
+					<search />
+				</b-navbar-brand>
 
-			<!-- to be right aligned in mobile -->
-			<b-collapse id="nav-collapse" is-nav align="right">
-				<b-navbar-nav>
-					<b-nav-item to="/news">الأخبار</b-nav-item>
-					<b-nav-item to="/university">الجامعة</b-nav-item>
-				</b-navbar-nav>
+				<b-nav-item-dropdown v-if="access" left no-caret>
+					<template #button-content>
+						الاعدادت
+					</template>
+					<b-dropdown-text class="text-right">
+						<b-button size="sm" variant="outline-info" class="mb-2">
+							<b-icon icon="person-fill" aria-hidden="true"></b-icon>
+							{{ username }}
+						</b-button></b-dropdown-text
+					>
+					<b-dropdown-text @click="logout" class="text-right">
+						<b-button size="sm" variant="outline-info">
+							تسجيل الخروج
+						</b-button>
+					</b-dropdown-text>
+				</b-nav-item-dropdown>
 
-				<!-- Right aligned nav items -->
-				<b-navbar-nav class="mr-auto">
-					<b-navbar-brand class="pl-5 mob-search">
-						<!-- <font-awesome-icon v-b-modal.modal-2 icon="search" size="lg" />
-
-            <b-modal id="modal-2" centered hide-footer hide-header> -->
-						<search />
-						<!-- </b-modal> -->
-					</b-navbar-brand>
-
-					<b-nav-item-dropdown v-if="access" left no-caret>
-						<template #button-content>
-							الاعدادت <b-icon icon="tools" aria-hidden="true"></b-icon>
-						</template>
-						<b-dropdown-text class="text-right">
-							<b-button size="sm" variant="outline-info" class="mb-2">
-								<b-icon icon="person-fill" aria-hidden="true"></b-icon>
-								{{ username }}
-							</b-button></b-dropdown-text
-						>
-						<b-dropdown-text @click="logout" class="text-right">
-							<b-button size="sm" variant="outline-info">
-								تسجيل الخروج
-							</b-button>
-						</b-dropdown-text>
-					</b-nav-item-dropdown>
-					<b-nav-item v-if="!access" to="/login">تسجيل الدخول</b-nav-item>
-					<b-nav-item v-if="!access" to="/register">انشاء حساب </b-nav-item>
-				</b-navbar-nav>
-			</b-collapse>
-		</b-navbar>
-	</div>
+				<b-nav-item v-if="!access" to="/login">تسجيل الدخول</b-nav-item>
+				<b-nav-item v-if="!access" to="/register">انشاء حساب </b-nav-item>
+			</b-navbar-nav>
+		</b-collapse>
+	</b-navbar>
 </template>
 
 <script>
