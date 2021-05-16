b19562c6ed47526c348e7b35530de006dae4eaf0
Murtaja <murtajaadnan7@gmail.com>
2021-05-16 12:03:09 +0300

    Refactor: refactor collage

--- src/views/Collage.vue
+++ src/views/Collage.vue
@@ -1,12 +1,12 @@
 <template>
-	<b-container align="right" fluid v-if="soruce !== ''">
+	<b-container align="right" fluid v-if="soruce">
 		<h4 class="mt-2">
 			{{ soruce.name }} <small>({{ $route.params.university }})</small>
 		</h4>
 		<hr />
 
 		<b-form-row>
-			<b-col align-self="stretch">
+			<b-col align-self="stretch" md="7" lg="8" xl="9">
 				<VueShowdown :markdown="soruce.description" flavor="github" />
 				<h6 class="mr-2">
 					الحدود الدنيا لاقسام {{ soruce.name }}
@@ -19,11 +19,9 @@
 
 				<hr class="col-sm-5 col-lg-4 col-xl-3 col-8 mr-2" align="right" />
 				<b-collapse visible appear id="collapse-3" class="border">
-					<b-table-simple responsive striped hover v-if="departments.length !== 0">
+					<b-table-simple responsive striped hover v-if="departments.length">
 						<b-thead>
 							<b-tr>
-								<!-- wider on small phones -->
-
 								<b-th>{{ soruce.name }}</b-th>
 								<b-th class="text-center" variant="secondary" colspan="5">الحدود الدنيا لسنة</b-th>
 							</b-tr>
@@ -34,9 +32,9 @@
 						</b-thead>
 						<b-tbody>
 							<b-tr v-for="n in soruce.departments_num" :key="n">
-								<b-td
+								<b-th
 									><b-link
-										class="linkColor"
+										class="linkColor department"
 										:to="
 											'/department/' +
 												$route.params.university +
@@ -46,7 +44,7 @@
 												departments[n - 1].name
 										"
 										>{{ departments[n - 1].name }}</b-link
-									></b-td
+									></b-th
 								>
 								<b-td v-for="i in years" :key="i">{{ departments[n - 1].sum[i] }} </b-td>
 							</b-tr>
@@ -58,7 +56,7 @@
 				</b-collapse>
 			</b-col>
 
-			<b-col align-self="start" md="4" lg="4" xl="3" class="border">
+			<b-col sm="12" md="5" lg="4" xl="3" class="border mt-2 mb-2">
 				<Table
 					:soruce="soruce"
 					:sideTableContent="sideTableContent"
@@ -67,19 +65,19 @@
 					:img="false"
 				/>
 			</b-col>
+			<b-col sm="12" lg="6">
+				<h6 class="mt-2">مراجعات {{ soruce.name }}</h6>
+				<hr class="col-md-3 col-sm-3 col-6" align="right"/>
+				<ReviewParent
+					:building="soruce.id"
+					sub_url="collage_reviews"
+					empty1="مراجعات"
+					empty2="المراجعين"
+			/></b-col>
 		</b-form-row>
-		<h6 class="mt-2">مراجعات {{ soruce.name }}</h6>
-		<hr class="col-md-3 col-sm-3 col-6" align="right" />
-		<ReviewParent
-			class="col-md-6 mt-2"
-			:building="soruce.id"
-			sub_url="collage_reviews"
-			empty1="مراجعات"
-			empty2="المراجعين"
-		/>
 	</b-container>
 	<div class="mt-5 d-flex justify-content-center" v-else>
-		<b-spinner style="width: 5rem; height: 5rem;" variant="primary"></b-spinner>
+		<b-spinner class="spinner" variant="primary"></b-spinner>
 	</div>
 </template>
 
@@ -132,6 +130,13 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+.spinner {
+	width: 5rem;
+	height: 5rem;
+}
+.department {
+	white-space: nowrap;
+}
 .box-top-bgc {
 	background-color: rgb(224, 134, 134);
 }
